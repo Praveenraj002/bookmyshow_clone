99f565d87f7f305d54a0c88a359ae9b35566cbb5
Praveenraj002 <praveenbose264@gmail.com>
2021-07-26 12:44:59 +0530

    adding Payment gateway

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,16 +1,31 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
+//componnets
+import PaymentModal from "../PaymentModal/Payment.component";
 
 //context
 import { MovieContext } from "../../context/movie.context";
 
 const MovieInfo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [price, setPrice] = useState(0);
   const {movie } = useContext(MovieContext);
 
 //optinal chaining
-  const genres = movie.genres?.map(({name}) => name).join(", ")
+  const genres = movie.genres?.map(({name}) => name).join(", ");
 
+  const rentMovies = () => {
+    setIsOpen(true);
+    setPrice(149);
+  };
+
+  const buyMovies = () => {
+    setIsOpen(true);
+    setPrice(149);
+    
+  }
   return (
     <>
+    <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price}/>
       <div className="flex flex-col gap-3 lg:gap-8">
         <div className="flex items-center gap-3 md:px-4">
           <div className="w-40 h-8">
@@ -33,11 +48,11 @@ const MovieInfo = () => {
             <h4>{(movie.runtime / 60).toFixed(2)} h &bull;{genres} &bull; 16+ &bull; 18 Mar, 2021</h4>
           </div>
           <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
-            <button className="bg-red-600 w-full py-3 text-white font-bold rounded-lg">
+            <button onClick={rentMovies} className="bg-red-600 w-full py-3 text-white font-bold rounded-lg">
               Rent ₹149
             </button>
-            <button className="bg-red-600 w-full py-3 text-white font-bold rounded-lg">
-              Buy ₹149
+            <button onClick={buyMovies} className="bg-red-600 w-full py-3 text-white font-bold rounded-lg">
+              Buy ₹499
             </button>
           </div>
         </div>
